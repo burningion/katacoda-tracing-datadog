19ac993c482383ac3cbeb2ec34976495898a7289
Kirk Kaiser <kirk.kaiser@datadoghq.com>
2019-01-31 09:42:21 -0500

    First trial in verification

--- distributed-tracing-datadog/step1-verify.sh
+++ distributed-tracing-datadog/step1-verify.sh
@@ -1,1 +1,1 @@
-(( $(docker ps -a | grep distributedtracingwithapmworkshop_frontend 2>/dev/null | wc -l) >= 1 )) && echo "done"
+(( $(docker ps -a | grep distributed 2>/dev/null | wc -l) >= 1 )) && echo "done"
