10a7aadd971d7c2c04d74ce0fe67e2f7f4dfc111
Kirk Kaiser <kirk.kaiser@datadoghq.com>
2019-02-22 10:50:32 -0500

    The repo has disappeared when in background.sh

--- distributed-tracing-datadog/foreground.sh
+++ distributed-tracing-datadog/foreground.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
+git clone https://github.com/burningion/distributed-tracing-with-apm-workshop /tracing-workshop
 cd /tracing-workshop
 echo Welcome to the APM Training Workshop!
