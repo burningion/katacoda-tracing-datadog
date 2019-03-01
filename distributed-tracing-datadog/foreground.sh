da4ac9dbd8cef5461d49f9ac733dff6e53190017
Kirk Kaiser <kirk.kaiser@datadoghq.com>
2019-03-01 17:19:32 -0500

    Clone bug seems gone. Remove duplicate.

--- distributed-tracing-datadog/foreground.sh
+++ distributed-tracing-datadog/foreground.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
-git clone https://github.com/burningion/distributed-tracing-with-apm-workshop /tracing-workshop
 cd /tracing-workshop
 echo Welcome to the APM Training Workshop!
