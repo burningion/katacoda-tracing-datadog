aa8c66c06ec2743b350fe2f87b8783899fde714e
Kirk Kaiser <kirk@zothcorp.com>
2019-03-25 09:58:25 -0400

    Add autocompletion to k8s in shell

--- autodiscovery-with-kubernetes/foreground.sh
+++ autodiscovery-with-kubernetes/foreground.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-kubeadm init --kubernetes-version $(kubeadm version -o short)
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+#kubeadm init --kubernetes-version $(kubeadm version -o short)
+#mkdir -p $HOME/.kube
+#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#sudo chown $(id -u):$(id -g) $HOME/.kube/config
+source <(kubektl completion bash)
