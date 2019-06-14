828c2e7b748a03971412a0e44fa78efa41ca5cb1
Kirk Kaiser <kirk@zothcorp.com>
2019-06-14 10:29:33 -0400

    Set up requirements

--- golang-kubernetes/foreground.sh
+++ golang-kubernetes/foreground.sh
@@ -1,3 +1,6 @@
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
 chmod +x skaffold
 mv skaffold /usr/local/bin
+git clone https://github.com/burningion/microservices-demo.git /hipster-shop
+cd /hipster-shop
+git checkout datadog-instrumented
