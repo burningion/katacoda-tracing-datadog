ab597473d31393a7e7691a9c6edf946ae4c030ae
Kirk Kaiser <kirk@zothcorp.com>
2019-07-08 16:21:54 -0400

    Change to k8s golang branch

--- golang-kubernetes-hands-on/background.sh
+++ golang-kubernetes-hands-on/background.sh
@@ -2,4 +2,4 @@
 mkdir /tracing-workshop
 git clone https://github.com/burningion/distributed-tracing-with-apm-workshop /tracing-workshop
 cd /tracing-workshop
-git checkout k8s-autodiscovery
+git checkout k8s-golang-workshop
