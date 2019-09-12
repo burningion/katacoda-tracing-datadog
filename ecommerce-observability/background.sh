0014c72a7a13a8d553b8a1c7ee58a9686bfba9e0
Kirk Kaiser <kirk@zothcorp.com>
2019-09-12 11:29:45 -0400

    Update to clean branch with no instrumentation

--- ecommerce-observability/background.sh
+++ ecommerce-observability/background.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /ecommerce-observability
 git clone https://github.com/burningion/ecommerce-observability /ecommerce-observability
-cd /ecommerce-observability
+cd /ecommerce-observability && git checkout no-instrumentation
